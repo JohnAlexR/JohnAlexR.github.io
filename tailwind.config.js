1ac3351eb13dcdd46b09c4a92525989e99117289
JohnAlexR <128446488+JohnAlexR@users.noreply.github.com>
2025-07-02 11:22:22 -0400

    mobile responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        horizon: ["Horizon", "sans-serif"],
+        "horizon-outlined": ["Horizon Outlined", "sans-serif"],
+      },
+      screens: {
+        xs: "360px",
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
